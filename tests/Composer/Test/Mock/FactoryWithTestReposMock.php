a18ec0e75efb40bfcc504a5e1aa5c1e241c0642c
fduch <alex.medwedew@gmail.com>
2014-06-09 13:47:18 +0400

    add InstallerWithCustomReposTest with fixtures in order to reproduce the bug with package that replaces package required by another package

---
+++ tests/Composer/Test/Mock/FactoryWithTestReposMock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Composer\Test\Mock;
+
+use Composer\Config;
+
+class FactoryWithTestReposMock extends FactoryMock{
+
+    public static function createConfig()
+    {
+        $config = new Config();
+
+        $config->merge(array(
+            'config' => array('home' => sys_get_temp_dir().'/composer-test'),
+            'repositories' => array(
+                'test_com' => array('type' => 'composer', 'url' => 'http://test'),
+                'packagist' => false
+            )
+        ));
+
+        return $config;
+    }
+}
+ 
